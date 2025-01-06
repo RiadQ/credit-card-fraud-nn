21e1d5b0b43707560dd8ee3c1afbcea134799c19
RiadQ <anessboutaleb@gmail.com>
2025-01-06 22:00:55 +0100

    Relates to #2: Added initial implementation of SMOTE

--- model.py
+++ model.py
@@ -49,6 +49,35 @@ true_labels = data.iloc[:, -1].values.astype(np.float32)
 
 num_columns = feature_vectors.shape[1]
 
+positive_indicies = np.where(true_labels == 1)[0]
+
+true_features = feature_vectors[positive_indicies]
+
+def calculate_nearest_neighbour(yTrue_vector):
+    distances = []
+    global true_features
+    for vector in true_features:
+        sums = 0
+        for i, value in enumerate(yTrue_vector):
+            sums += (value - vector[i])**2
+        
+        sums = np.sqrt(sums)
+        distances.append(sums)
+
+    return distances
+
+
+def synthetic_sample(vector, k=5):
+    nearest = sorted_pairs[:k]
+    enumerated = list(enumerate(calculate_nearest_neighbour(vector)))
+    sorted_pairs = sorted(enumerated, key=lambda x: x[1])
+    l = np.random.uniform(0, 1)
+    for d in nearest:
+        global feature_vectors, true_labels
+        n = true_features[d[0]]
+        synthetic = vector + l * (n - vector)
+        feature_vectors = np.vstack([feature_vectors, synthetic])
+        true_labels = np.append(true_labels, 1)
 
 
 class hiddenLayer:
